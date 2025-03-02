f3efe68b3e69e731a909dad79afc14744094998d
lolpop1228 <plugamerzaza@gmail.com>
2025-03-02 18:34:23 +0700

    Interact System

---
+++ COMPOTER/Assets/Scripts/MyRoom/TurnPCOn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TurnPCOn : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("Kuy");
+    }
+}
