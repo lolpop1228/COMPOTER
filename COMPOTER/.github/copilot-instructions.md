203183315684ed1e9dcc3aa0dec3d93e427a8cc0
lolpop1228 <plugamerzaza@gmail.com>
2025-03-04 20:38:29 +0700

    cutscene

--- COMPOTER/.github/copilot-instructions.md
+++ COMPOTER/.github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: COMPOTER
 - Unity version: Unity 2022.3.48f1
 - Active game object:
-  - Name: DialogueBox
-  - Tag: Untagged
+  - Name: Main Camera
+  - Tag: MainCamera
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- COMPOTER/Assets/Scripts/System/OpenCutscene.cs
+++ COMPOTER/Assets/Scripts/System/OpenCutscene.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class OpenCutscene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayableDirector playableDirector;
+    public PlayerMovement playerMovement;
+    public Camera targetCamera;
+    public Vector3 targetRotation = new Vector3(0f, 0f, 0f);
+
+    private void Start()
     {
-        
+        if (playableDirector != null)
+        {
+            playableDirector.Play();
+            playableDirector.stopped += OnTimelineEnd;
+        }
+
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTimelineEnd(PlayableDirector director)
     {
-        
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = true;
+        }
+
+        if (targetCamera != null)
+        {
+            targetCamera.transform.rotation = Quaternion.Euler(targetRotation);
+        }
+
+        playableDirector.stopped -= OnTimelineEnd;
     }
 }
