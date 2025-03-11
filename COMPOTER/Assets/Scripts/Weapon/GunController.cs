acd36c66f1892b5058ca7424b3a067909beae0a8
lolpop1228 <plugamerzaza@gmail.com>
2025-03-11 19:21:02 +0700

    Gun

--- COMPOTER/Assets/Scripts/Weapon/GunController.cs
+++ COMPOTER/Assets/Scripts/Weapon/GunController.cs
@@ -1,45 +1,37 @@
 using UnityEngine;
+using System.Collections;
 
 public class GunController : MonoBehaviour
 {
-    public float damage = 10f;
-    public float range = 100f;
-    public float impactForce = 30f;
-
-    public Camera fpsCam;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 500f;
+    public float fireRate = 0.5f;
+    public Transform muzzlePoint;
     public ParticleSystem muzzleFlash;
-    public GameObject impactEffect;
+
+    private float nextTimeToFire;
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + fireRate;
             Shoot();
-        }
+        }    
     }
 
     void Shoot()
     {
         muzzleFlash.Play();
 
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        GameObject bullet = Instantiate(bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        if (rb != null)
         {
-            Debug.Log(hit.transform.name);
-
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null)
-            {
-                target.TakeDamage(damage);
-            }
-
-            if (hit.rigidbody != null)
-            {
-                hit.rigidbody.AddForce(-hit.normal * impactForce);
-            }
-
-            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGO, 2f);
+            rb.velocity = muzzlePoint.forward * bulletSpeed;
         }
+
+        Destroy(bullet, 2f);
     }
 }
