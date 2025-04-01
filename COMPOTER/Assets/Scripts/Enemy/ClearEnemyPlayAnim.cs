593e2fafe72e64078d951e60a269e7c63f4fc69f
lolpop1228 <plugamerzaza@gmail.com>
2025-04-01 17:35:49 +0700

    GunFight4

---
+++ COMPOTER/Assets/Scripts/Enemy/ClearEnemyPlayAnim.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearEnemyPlayAnim : MonoBehaviour
+{
+    public Animator animator;
+    public string animToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount <= 0)
+        {
+            if (animator != null)
+            {
+                animator.Play(animToPlay);
+            }
+        }
+    }
+}
