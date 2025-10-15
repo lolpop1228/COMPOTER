1630bbc9979743a775e9ee9274a6ab034c5f10e1
zJTGodz <noppawat2530@gmail.com>
2025-10-15 17:36:49 +0700

    new title + collide w/portal

--- COMPOTER/Assets/Scripts/System/GoNextScene.cs
+++ COMPOTER/Assets/Scripts/System/GoNextScene.cs
@@ -2,13 +2,28 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoNextScene : MonoBehaviour, IInteractable
+public class GoNextScene : MonoBehaviour
 {
     public string sceneToGo;
+    public CharacterController playerController;
 
-    public void Interact()
+    void TeleportPlayer()
     {
+        playerController.enabled = false;
+
         SceneManager.LoadScene(sceneToGo);
+
+        playerController.enabled = true;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (playerController != null)
+            {
+                TeleportPlayer();
+            }
+        }
+    }
 }
